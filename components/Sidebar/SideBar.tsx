206655d531c3b8c55185587c6885f9d151854dc0
Aditya Sood <aditya12sc@gmail.com>
2023-07-20 02:38:23 +0530

    Update SideBar.tsx

--- components/Sidebar/SideBar.tsx
+++ components/Sidebar/SideBar.tsx
@@ -5,7 +5,8 @@ import { useCollection } from "react-firebase-hooks/firestore";
 import { collection, orderBy, query } from "firebase/firestore";
 import { db } from "@/firebase/firebase";
 import NewChat from "../Chat/NewChat";
-// import ChatRow from "../Chat/ChatRow";
+import ChatRow from "./ChatRow";
+import ModelSelection from "../ModelSelection/ModelSelection";
 
 function SideBar() {
   const { data: session } = useSession();
@@ -23,12 +24,22 @@ function SideBar() {
         <div>
           {/* New Chat Button */}
           <NewChat />
-          <div>{/* Model Selection Dropdown */}</div>
+          <div>
+            {/* Model Selection Dropdown */}
+            <ModelSelection />
+          </div>
 
-          {/* Mapping through chats stored in DB */}
-          {/* {chats?.docs.map((chat) => (
+          <div className="flex flex-col space-y-2 my-2">
+            {loading && (
+              <div className="animate-pulse text-center text-white">
+                <p>Loading Chats...</p>
+              </div>
+            )}
+            {/* Map through the chat rows */}
+            {chats?.docs.map((chat) => (
               <ChatRow key={chat.id} id={chat.id} />
-            ))} */}
+            ))}
+          </div>
         </div>
       </div>
 
