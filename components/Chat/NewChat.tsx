74cdeddb140b6c421b8ff4e152931eb86bb8a4a0
Aditya Sood <aditya12sc@gmail.com>
2023-07-19 19:54:07 +0530

    Update NewChat.tsx
    - implemented the "create new chat" functionality

--- components/Chat/NewChat.tsx
+++ components/Chat/NewChat.tsx
@@ -1,13 +1,35 @@
+'use client';
+
 import { PlusIcon } from "@heroicons/react/24/outline";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "@/firebase/firebase";
 
 function NewChat() {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  // create a new chat
+  const createNewChat = async () => {
+    const doc = await addDoc(
+      collection(db, "users", session?.user?.email!, "chats"),
+      {
+        userId: session?.user?.email!,
+        createdAt: serverTimestamp(),
+      }
+    );
+
+    //opens a new chat window
+    router.push(`/chat/${doc.id}`);
+  };
 
   return (
-    <div className="border-gray-700 border chatRow">
+    <div onClick={createNewChat} className="border-gray-700 border chatRow">
       <PlusIcon className="h-4 w-4" />
       <p>New Chat</p>
     </div>
-  )
+  );
 }
 
-export default NewChat
+export default NewChat;
