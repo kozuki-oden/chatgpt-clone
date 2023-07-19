8a64c7a07470ddefde3a533f07227241f53021d5
Aditya Sood <aditya12sc@gmail.com>
2023-07-20 02:37:30 +0530

    Create ModelSelection.tsx
    - implemented the model selection feature

---
+++ components/ModelSelection/ModelSelection.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import useSWR from "swr";
+import Select from "react-select";
+
+const fetchModels = () => fetch("/api/getEngines").then((res) => res.json());
+
+const { data: models, isLoading } = useSWR("models", fetchModels);
+
+const { data: model, mutate: setModel } = useSWR("model", {
+  fallbackData: "text-davinci-003",
+});
+
+function ModelSelection() {
+  return (
+    <div className="mt-2">
+      <Select
+        className="mt-2"
+        options={models?.modelOptions}
+        isSearchable
+        isLoading={isLoading}
+        menuPosition="fixed"
+        placeholder={model}
+        defaultValue={model}
+        onChange={(e) => setModel(e.value)}
+      />
+    </div>
+  );
+}
+
+export default ModelSelection;
