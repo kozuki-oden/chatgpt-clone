cfa1e3a327a720599d3598e254823faec621dfa1
Aditya Sood <aditya12sc@gmail.com>
2023-07-20 02:33:19 +0530

    Update page.tsx
    - integrated the chat window
    - integrated the input tab

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,7 +1,23 @@
-function ChatPage() {
+import ChatWindow from "@/components/Chat/ChatWindow";
+import InputTab from "@/components/Chat/InputTab";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+function ChatPage({ params: { id } }: Props) {
   return (
-    <div>ChatPage</div>
-  )
+    <div className="flex flex-col overflow-hidden h-screen">
+      
+      {/* Chat Window*/}
+      <ChatWindow chatId={id} />
+      
+      {/* Input Tab */}
+      <InputTab chatId={id} />
+    </div>
+  );
 }
 
 export default ChatPage;
