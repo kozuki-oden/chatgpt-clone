502954b89381aeae954adbac0bc0cabb01ec67e1
Aditya Sood <aditya12sc@gmail.com>
2023-07-20 18:22:55 +0530

    Update firebase.ts

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: "AIzaSyA4dm0PDm2Qr2LVe_a3rs0X5COU4-7ofhg",
   authDomain: "chatgpt-clone-1.firebaseapp.com",
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  projectId: "chatgpt-clone-1",
   storageBucket: "chatgpt-clone-1.appspot.com",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  messagingSenderId: "26748875357",
+  appId: "1:26748875357:web:6bf3158c4ba7e2c94f13b4"
 };
 
 // Initialize Firebase
